e2087bd315949bb6bad59b1459ca86b4eaccf22f
ilyapatis24 <ilyapatis24@mail.ru>
2024-10-31 22:16:52 +0300

    init

---
+++ src/js/validator.js
@@ -0,0 +1,12 @@
+"use strict";
+
+export default class Validator {
+    constructor(username) {
+        this.username = username
+    }
+
+    validateUsername() {
+        return /^[a-z]([\w-]*[a-z])?$/i.test(this.username) && !/\d{4}/.test(this.username);
+    }
+
+}
